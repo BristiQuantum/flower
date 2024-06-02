4a4ed57414cc1ed8c28dab2ed2eb9400b52260bd
BristiQuantum <bristi.get.contact@gmail.com>
2024-06-02 09:38:14 +0600

    add modiaquery for silder

--- src/page/Home_Page/BestCollection/BestColection.jsx
+++ src/page/Home_Page/BestCollection/BestColection.jsx
@@ -7,23 +7,24 @@ import { Navigation } from 'swiper/modules';
 
 const BestColection = () => {
 
-const getBreakpoints = () => {
-  return {
-    640: {
-      slidesPerView: 2, // 2 slides per view on screens >= 640px
-      spaceBetween: 20,
-    },
-    1024: {
-      slidesPerView: 3, // 3 slides per view on screens >= 1024px
-      spaceBetween: 30,
-    },
-  };
-};
+
+    const getSlidesPerView = () => {
+        const width = window.innerWidth;
+        if (width < 450) {
+            return 1;
+        } else if (width >= 450 && width < 640) {
+            return 2;
+        } else if (width >= 640 && width < 786) {
+            return 3;
+        } else {
+            return 3;
+        }
+    };
 
 
     return (
-        <div className="py-24 px-32">
-            <div className="flex items-center justify-between">
+        <div className="py-24 px-2 lg:px-20 2xl:px-32">
+            <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between space-y-4 sm:space-y-0">
                 <h2>Best Collection</h2>
 
                 <button className="btn-not-fill">All Collection</button>
@@ -33,112 +34,113 @@ const getBreakpoints = () => {
             <div className="">
 
                 <Swiper
-                    slidesPerView={getBreakpoints()}
+                    slidesPerView={getSlidesPerView()}
                     spaceBetween={30}
                     navigation={true} modules={[Navigation]}
                     className="mySwiper"
-                    
+
                 >
-                   
-                        <SwiperSlide>
-                            <div className="rounded-2xl w-fit p-9 relative ">
-                                <div className="w-52 h-72 mb-5">
-                                    <img src={c1} alt="" />
-                                </div>
 
-                                <div className="text-center">
-                                    <h3>Tulip Flower Boki</h3>
-                                    <h4>Tulip Flower</h4>
-                                    <p className='font-bold'>$60.8</p>
-                                </div>
-
-                                <div className="badge-best">
-                                    <p>BEST</p>
-                                </div>
+                    <SwiperSlide>
+                        <div className="rounded-2xl w-fit p-9 relative ">
+                            <div className="w-36 md:w-52 h-60 md:h-72 mb-5">
+                                <img src={c1} alt="" />
                             </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <div className="rounded-2xl w-fit p-9 relative ">
-                                <div className="w-52 h-72 mb-5">
-                                    <img src={c1} alt="" />
-                                </div>
 
-                                <div className="text-center">
-                                    <h3>Tulip Flower Boki</h3>
-                                    <h4>Tulip Flower</h4>
-                                    <p className='font-bold'>$60.8</p>
-                                </div>
-
+                            <div className="text-center">
+                                <h3>Tulip Flower Boki</h3>
+                                <h4>Tulip Flower</h4>
+                                <p className='font-bold'>$60.8</p>
                             </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <div className="rounded-2xl w-fit p-9 relative ">
-                                <div className="w-52 h-72 mb-5">
-                                    <img src={c1} alt="" />
-                                </div>
 
-                                <div className="text-center">
-                                    <h3>Tulip Flower Boki</h3>
-                                    <h4>Tulip Flower</h4>
-                                    <p className='font-bold'>$60.8</p>
-                                </div>
-
-                                <div className="badge-new">
-                                    <p>NEW</p>
-                                </div>
+                            <div className="badge-best">
+                                <p>BEST</p>
                             </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <div className="rounded-2xl w-fit p-9 relative ">
-                                <div className="w-52 h-72 mb-5">
-                                    <img src={c1} alt="" />
-                                </div>
-
-                                <div className="text-center">
-                                    <h3>Tulip Flower Boki</h3>
-                                    <h4>Tulip Flower</h4>
-                                    <p className='font-bold'>$60.8</p>
-                                </div>
-
-                                <div className="badge-best">
-                                    <p>BEST</p>
-                                </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="rounded-2xl w-fit p-9 relative ">
+                            <div className="w-36 md:w-52 h-60 md:h-72 mb-5">
+                                <img src={c1} alt="" />
                             </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <div className="rounded-2xl w-fit p-9 relative ">
-                                <div className="w-52 h-72 mb-5">
-                                    <img src={c1} alt="" />
-                                </div>
 
-                                <div className="text-center">
-                                    <h3>Tulip Flower Boki</h3>
-                                    <h4>Tulip Flower</h4>
-                                    <p className='font-bold'>$60.8</p>
-                                </div>
-
-                                <div className="badge-new">
-                                    <p>NEW</p>
-                                </div>
-
+                            <div className="text-center">
+                                <h3>Tulip Flower Boki</h3>
+                                <h4>Tulip Flower</h4>
+                                <p className='font-bold'>$60.8</p>
                             </div>
-                        </SwiperSlide>
-                        <SwiperSlide>
-                            <div className="rounded-2xl w-fit p-9 relative ">
-                                <div className="w-52 h-72 mb-5">
-                                    <img src={c1} alt="" />
-                                </div>
 
-                                <div className="text-center">
-                                    <h3>Tulip Flower Boki</h3>
-                                    <h4>Tulip Flower</h4>
-                                    <p className='font-bold'>$60.8</p>
-                                </div>
-
-
+                            
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="rounded-2xl w-fit p-9 relative ">
+                            <div className="w-36 md:w-52 h-60 md:h-72 mb-5">
+                                <img src={c1} alt="" />
                             </div>
-                        </SwiperSlide>
-                  
+
+                            <div className="text-center">
+                                <h3>Tulip Flower Boki</h3>
+                                <h4>Tulip Flower</h4>
+                                <p className='font-bold'>$60.8</p>
+                            </div>
+
+                            <div className="badge-new">
+                                <p>New</p>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="rounded-2xl w-fit p-9 relative ">
+                            <div className="w-36 md:w-52 h-60 md:h-72 mb-5">
+                                <img src={c1} alt="" />
+                            </div>
+
+                            <div className="text-center">
+                                <h3>Tulip Flower Boki</h3>
+                                <h4>Tulip Flower</h4>
+                                <p className='font-bold'>$60.8</p>
+                            </div>
+
+                            <div className="badge-new">
+                                <p>New</p>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="rounded-2xl w-fit p-9 relative ">
+                            <div className="w-36 md:w-52 h-60 md:h-72 mb-5">
+                                <img src={c1} alt="" />
+                            </div>
+
+                            <div className="text-center">
+                                <h3>Tulip Flower Boki</h3>
+                                <h4>Tulip Flower</h4>
+                                <p className='font-bold'>$60.8</p>
+                            </div>
+
+                            
+                        </div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <div className="rounded-2xl w-fit p-9 relative ">
+                            <div className="w-36 md:w-52 h-60 md:h-72 mb-5">
+                                <img src={c1} alt="" />
+                            </div>
+
+                            <div className="text-center">
+                                <h3>Tulip Flower Boki</h3>
+                                <h4>Tulip Flower</h4>
+                                <p className='font-bold'>$60.8</p>
+                            </div>
+
+                            <div className="badge-best">
+                                <p>Best</p>
+                            </div>
+                        </div>
+                    </SwiperSlide>
+
+
                 </Swiper>
 
 
